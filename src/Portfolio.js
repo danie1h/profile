ecd376a2a1d1ff5d5e14805a6d1ed99ef1bec4a8
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-10-24 15:02:32 -0700

    Add Portfolio icon and title

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -66,6 +66,10 @@ class Portfolio extends Component {
 
     return (
       <div className='portfolio' id='portfolio'>
+        <div className='portfolio-header'>
+          <img className='portfolio-icon' src='https://res.cloudinary.com/joeschmoe1/image/upload/v1540416981/portfolio-icon.png' alt='portfolio-icon' />
+          <h2 className='portfolio-title'>Portfolio</h2>
+        </div>
         <div className='d3-pen-container'>
           {d3PenListInJSX}
         </div>
